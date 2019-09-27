cf30fa952a1581ef52ec2a330f831173b4dd2145
Michael Roth <mail@mroth.net>
2019-09-27 22:22:54 +0200

    Utf-8 encoding on loading package.json

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
-const { version } = JSON.parse(readFileSync(join(__dirname, "../package.json")).toString());
+const path = join(__dirname, "../package.json")
+const { version } = JSON.parse(readFileSync(path, {encoding: "utf-8"}));
 
 export default version;
