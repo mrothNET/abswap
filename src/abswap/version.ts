a4058c7152aee657e9ca65872b4fc91e814e58b5
Michael Roth <mail@mroth.net>
2020-01-24 22:52:20 +0100

    Fix --version option

--- src/abswap/version.ts
+++ src/abswap/version.ts
@@ -5,7 +5,7 @@ let version: string | undefined;
 
 export async function getVersion(): Promise<string> {
   if (!version) {
-    const path = join(__dirname, "../package.json");
+    const path = join(__dirname, "../../package.json");
     const json = await readFile(path, { encoding: "utf-8" });
     version = JSON.parse(json).version;
   }
